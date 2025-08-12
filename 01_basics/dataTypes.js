852b4923d13b914703a457a4f8d15e9f5e02e474
Ravi9344 <137464792+Ravi9344@users.noreply.github.com>
2025-08-12 17:24:34 +0000

    Conversion of dataTypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -5,3 +5,32 @@
 console.log(3
     +
     4); // code readability should  be high, so this is not recommended
+
+
+// let age = "33"
+// console.log(typeof age);
+
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 ; false => 0 
+
+let isLoggedIn = "Ravi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Ravi" => true
+
+
+let num = 100
+let stringNumber = String(num)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
+
+
